144b2135c10e7e446af42c91441d9150f05b9023
Alexey Ramazanov <alexey.ramazanov@azati.com>
2010-06-07 17:51:44 +0300

    sugarce_funambol ssl fix

--- cookbooks/sugarce_funambol/recipes/remove_ssl.rb
+++ cookbooks/sugarce_funambol/recipes/remove_ssl.rb
@@ -1,1 +1,9 @@
 include_recipe "apache2::ssl_disable"
+
+bash "remove_certificate" do
+  code <<-EOH
+#{node[:sugarce_funambol][:funambol_data_dir]}/Funambol/tools/jre-1.6.0/jre/bin/keytool -delete -alias sugarce -storepass changeit -noprompt -keystore #{node[:sugarce_funambol][:funambol_data_dir]}/Funambol/ds-server/lib/security/cacerts
+/etc/init.d/funambol stop
+/etc/init.d/funambol start
+EOH
+end
--- cookbooks/sugarce_funambol/recipes/setup_ssl.rb
+++ cookbooks/sugarce_funambol/recipes/setup_ssl.rb
@@ -3,3 +3,14 @@ node[:apache][:certificates][:cert]         = node[:params][:certificate]
 node[:apache][:certificates][:ca_cert]      = node[:params][:ca_certificate]
 
 include_recipe "apache2::ssl_enable"
+
+bash "install_certificate" do
+  code <<-EOH
+cd /mnt
+echo "#{node[:params][:certificate]}" > sugarcecert.crt
+#{node[:sugarce_funambol][:funambol_data_dir]}/Funambol/tools/jre-1.6.0/jre/bin/keytool -import -alias sugarce -file sugarcecert.crt -storepass changeit -noprompt -keystore #{node[:sugarce_funambol][:funambol_data_dir]}/Funambol/ds-server/lib/security/cacerts
+rm sugarcecert.crt
+/etc/init.d/funambol stop
+/etc/init.d/funambol start
+EOH
+end
