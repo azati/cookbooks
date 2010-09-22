10dc0b413186a63cf3b44032561dce434b7559a0
Alexey Ramazanov <alexey.ramazanov@azati.com>
2010-09-22 14:15:25 +0300

    fixed nginx recipe

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -12,33 +12,33 @@ groupadd nginx && useradd -g nginx nginx
 EOH
 end
 
-remote_file "/mnt/nginx-0.7.64.tar.gz" do
-  source "nginx-0.7.64.tar.gz"
+remote_file "/mnt/nginx-0.7.67.tar.gz" do
+  source "http://data.azati.s3.amazonaws.com/nginx/nginx-0.7.67.tar.gz"
 end
 remote_file "/mnt/gnosek-nginx-upstream-fair-2131c73.tar.gz" do
-  source "gnosek-nginx-upstream-fair-2131c73.tar.gz"
+  source "http://data.azati.s3.amazonaws.com/nginx/gnosek-nginx-upstream-fair-2131c73.tar.gz"
 end
 remote_file "/mnt/ngx_http_auth_pam_module-1.1.tar.gz" do
-  source "ngx_http_auth_pam_module-1.1.tar.gz"
+  source "http://data.azati.s3.amazonaws.com/nginx/ngx_http_auth_pam_module-1.1.tar.gz"
 end
 
 bash "compile_nginx" do
   code <<-EOH
 cd /mnt
-tar -xzf nginx-0.7.64.tar.gz
+tar -xzf nginx-0.7.67.tar.gz
 tar -xzf gnosek-nginx-upstream-fair-2131c73.tar.gz
 tar -xzf ngx_http_auth_pam_module-1.1.tar.gz
-cd nginx-0.7.64
+cd nginx-0.7.67
 ./configure --with-http_stub_status_module --with-http_gzip_static_module --with-pcre --with-http_ssl_module --without-http_fastcgi_module --user=nginx --group=nginx --add-module=/mnt/gnosek-nginx-upstream-fair-2131c73 --add-module=/mnt/ngx_http_auth_pam_module-1.1
 make && make install
 EOH
 end
 
-directory "/mnt/nginx-0.7.64" do
+directory "/mnt/nginx-0.7.67" do
   recursive true
   action :delete
 end
-file "/mnt/nginx-0.7.64.tar.gz" do
+file "/mnt/nginx-0.7.67.tar.gz" do
   action :delete
 end
 file "/mnt/gnosek-nginx-upstream-fair-2131c73.tar.gz" do
